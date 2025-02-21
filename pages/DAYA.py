d233e055dd788ad30364f7f3b9b179b40e9fcc86
RAYHAN25123 <155223792+RAYHAN25123@users.noreply.github.com>
2025-02-21 15:58:37 +0700

    Update and rename Daya.py to DAYA.py

--- pages/DAYA.py
+++ pages/DAYA.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
 st.title("Daya")
-st.subheader("P = Daya (Watt)"
-             "W = Usaha (Joule)"
-             "T = Waktu (Sekon/Detik)")
+st.markdown("""
+- P = Daya (Watt)"
+- W = Usaha (Joule)
+- T = Waktu (Sekon/Detik)
+""")
 st.subheader("P = W/T")
 f = st.number_input("Masukan Gayanya(Newton)")
 s = st.number_input("Masukan Perpindahan Bendanya(Meter)")
-W = f * s
+w = f * s
 st.header(w)
 
 t = st.number_input("Masukan Waktunya(sekon/Detik)")
