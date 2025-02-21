801c3364c4d93f6bee207c3e8245ea4c92d10137
RAYHAN25123 <155223792+RAYHAN25123@users.noreply.github.com>
2025-02-21 14:48:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,17 @@ st.markdown("""
 Gampang! Cuman pilih lewat sidebar di sini👈 terus tinggal satset satset deh.
 """)
 
+st.title("BANGUN RUANG BALOK")
+p = st.number_input("Masukan Nilai Panjang:")
+l = st.number_input("Masukan Nilai Lebar:")
+t = st.number_input("Masukan Nilai Tinggi:")
+st.header("KELILING")
+k = 4 * ( p + l + t )
+st.subheader(k)
+st.header("LUAS BALOK")
+l = ( p * l ) + ( p * t ) + ( l * t )
+st.subheader(l)
+st.header("VOLUME BALOK")
+v = p * l * t
+st.subheader(v)
+
