d0effee0ea02661d82530f9b4b2c5e3603709393
RAYHAN25123 <155223792+RAYHAN25123@users.noreply.github.com>
2025-02-21 14:40:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,33 +13,9 @@ st.markdown("""
     Orang yang terlibat :
         - Raditya Rayhan Anjaya sebagai Programmer
         - Khalil Ghibran Dzakwan Mulyadi sebagai Penyusun
-
 ### Apa Itu KAKURU?
     **KAKURU** adalah sebuah mesin kalkulator yang berkerja layaknya kalkulator pada umumnya, hanya saja dirancang untuk mempermudah menyelesaikan masalah mengenai rumus matematika.
     ### Gimana cara kerjanya?
     Gampang! Cuman pilih lewat sidebar di sini👈 terus tinggal satset satset deh.
     """)
-
-st.subheader(":mailbox: Gak nemu rumus? Bilang aja!")
-st.write("Website ini masih sepenuhnya dalam pengembangan, jika ada bug atau kesalahan, bisa kontak developer lewat form dibawah👇")
-
-contact_form = """
-<form action="https://formsubmit.co/0e4a07a9d07e5a315c5cb8aaae8d758c" method="POST">
-     <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Nama Kamu" required>
-     <input type="email" name="email" placeholder="Email Kamu" required>
-     <textarea name="message" placeholder="Pesan kamu disini! (Bisa berupa saran atau error!)"></textarea>
-     <button type="submit">Send</button>
-</form>
-"""
-
-st.markdown(contact_form, unsafe_allow_html=True)
-
-# Use Local CSS File
-def local_css(file_name):
-   with open(file_name) as f:
-      st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-local_css("./style.css")
 
