8bc37a2077c77bee29098a2d98cbe9d21fb32c01
RAYHAN25123 <155223792+RAYHAN25123@users.noreply.github.com>
2025-02-21 14:35:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,8 @@ st.sidebar.success("👆SILAHKAN PILIH👆")
 st.title("KAKURU (Kalkulator Rumus): Cara mudah menghitung cepat soal matematika")
 st.markdown("""
     Orang yang terlibat :
-    
-        - Raditya Rayhan Anjaya sebagai Programmer
-        
-        - Khalil Ghibran Dzakwan Mulyadi sebagai Penyusun
+    - Raditya Rayhan Anjaya sebagai Programmer
+    - Khalil Ghibran Dzakwan Mulyadi sebagai Penyusun
 ### Apa Itu KAKURU?
     **KAKURU** adalah sebuah mesin kalkulator yang berkerja layaknya kalkulator pada umumnya, hanya saja dirancang untuk mempermudah menyelesaikan masalah mengenai rumus matematika.
     ### Gimana cara kerjanya?
