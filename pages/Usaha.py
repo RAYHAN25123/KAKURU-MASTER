07f3d4791531236778796c8686052f164ce488a2
RAYHAN25123 <155223792+RAYHAN25123@users.noreply.github.com>
2025-02-21 15:16:13 +0700

    Update Usaha.py

--- pages/Usaha.py
+++ pages/Usaha.py
@@ -6,7 +6,10 @@ st.markdown("""
 - F = Gaya (Newton)
 - S = Perpindahan Benda (Meter)
              """)
-st.subheader("Rumus = w = f x s")
+st.title("Rumus")
+st.markdown("""
+- W = F x S
+""")
 f = st.number_input("Masukan Gaya(Newton)")
 s = st.number_input("Perpindahan Benda(Meter)")
 w = f * s
