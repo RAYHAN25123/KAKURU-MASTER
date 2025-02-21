5de2b28ca89bc818dac679f711638bfdbe77ada0
RAYHAN25123 <155223792+RAYHAN25123@users.noreply.github.com>
2025-02-21 16:21:51 +0700

    Update and rename Energi_Potensial.py to ENERGI_POTENSIAL.py

--- pages/ENERGI_POTENSIAL.py
+++ pages/ENERGI_POTENSIAL.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
 st.title("Energi Potensial")
-st.subheader("EP = Energi Potensial"
-             "M = Massa Benda (Kg)"
-             "G = Percepatan Gravitasi (m/s)"
-             "H = Ketinggian")
+st.markdown("""
+- EP = Energi Potensial"
+- M = Massa Benda (Kg)
+- G = Percepatan Gravitasi (m/s)
+- H = Ketinggian")
 st.subheader("Rumus = m x g x h")
 
 m = st.number_input("Masukan Massanya")
 h = st.number_input("Masukan Tingginya")
 ep = m * 10 * h
+st.subheader("Hasil")
 st.header(ep)
