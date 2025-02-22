e3476183f1cc88ceee06442cf94943fe67eb530b
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-02-22 14:54:36 +0700

    Update HUKUM_OHM.py

--- pages/HUKUM_OHM.py
+++ pages/HUKUM_OHM.py
@@ -17,7 +17,7 @@ st.subheader(v)
 st.header("Rumus Mencari Ampere")
 st.subheader("I = V/R")
 v1 = st.number_input("Masukan Tegangan Listriknya(Volt)")
-r1 = st.number_input("Masukkan Hambatan Listrik (Ω)", key = r1)
+r1 = st.number_input("Masukkan Hambatan Listrik (Ω)", key ="r1")
 try:
     i1 = v1 / r1
     st.write(f"Arus listrik (I) = {i1} A")
