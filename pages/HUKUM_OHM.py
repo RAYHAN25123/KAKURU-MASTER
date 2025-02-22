cde81163e10d85b6f760d9936b2200560a7a0673
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-02-22 14:52:24 +0700

    Update HUKUM_OHM.py

--- pages/HUKUM_OHM.py
+++ pages/HUKUM_OHM.py
@@ -17,6 +17,11 @@ st.subheader(v)
 st.header("Rumus Mencari Ampere")
 st.subheader("I = V/R")
 v1 = st.number_input("Masukan Tegangan Listriknya(Volt)")
-    r1 = st.number_input("Masukkan Hambatan Listrik (Ω)", min_value=0.01)
+    r1 = st.number_input("Masukkan Hambatan Listrik (Ω)", key = r1)
+try:
+    i1 = v1 / r1
+    st.write(f"Arus listrik (I) = {i1} A")
+except ZeroDivisionError:
+    st.error("⚠️ Error: Tidak bisa membagi dengan nol! Harap masukkan hambatan yang lebih besar dari 0.")
 st.header("Ampere")
 st.subheader(i1)
