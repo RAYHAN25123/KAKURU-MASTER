8ee984b607792396ed6c63792f5ba18be221a27d
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-03-05 16:56:36 +0700

    Update Kecepatan_Gelombang.py

--- pages/Kecepatan_Gelombang.py
+++ pages/Kecepatan_Gelombang.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
 st.title("Kecepatan Gelombang")
-st.header("Rumus")
+st.markdown("""
+- v = kecepatan gelombang (m/s)
+- f = frekuensi gelombang (Hz atau 1/s)
+- λ = panjang gelombang (m)
+""")
+st.subheader("Rumus")
 st.markdown("""
 - v=fλ
 """)
