1218a7b77b0930afbf1fb9b685c157cf80a41a0c
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-03-07 14:19:34 +0700

    Create HUKUM_KEKEKALAN_ENERGI.py

---
+++ pages/HUKUM_KEKEKALAN_ENERGI.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Hukum Kekekalan Energi")
+st.markdown("""
+- Em = Energi Mekanik (Joule)
+- Ek = Energi Kinetik (Joule)
+- Ep = Energi Potensial (Joule)
+""")
+st.header("Rumus")
+st.markdown("""
+- Em : Ek x Ep
+""")
+st.header("Energi Mekanik")
+st.markdown("""
+- Mencari Energi Kinetik
+""")
